2edc68fc4e58d5ba565c360801bb6b8222792e92
Swastik Adhikari <78269644+sskadk@users.noreply.github.com>
2024-07-21 17:25:46 +0545

    QrCodeGenerator

---
+++ qr_code_generator.py
@@ -0,0 +1,9 @@
+import qrcode
+from PIL import Image
+
+qr=qrcode.QRCode(version=1,
+                 error_correction=qrcode.constants.ERROR_CORRECT_H, box_size=18, border=4,)
+qr.add_data("Enter your data to make a QR")
+qr.make(fit=True)
+img=qr.make_image(fill_color="red", back_color="blue")
+img.save("NewQr.png")

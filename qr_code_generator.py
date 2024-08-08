458a8c1883c4bb543f1638266fb9424b5fd4e53d
Swastik Adhikari <78269644+sskadk@users.noreply.github.com>
2024-08-08 22:32:14 +0545

    Update qr_code_generator.py

--- qr_code_generator.py
+++ qr_code_generator.py
@@ -3,6 +3,7 @@ Note: Before running this program
 QR Module and Image Module
 should be installed
 '''
+
 import qrcode           #QR Module
 from PIL import Image   #Image Module  
 data=input("Enter Data of which you want to make QrCode:")
@@ -13,4 +14,5 @@ qr.add_data(f"{data}")
 qr.make(fit=True)
 img=qr.make_image(fill_color="white", back_color="black")
 img.save(f"{qrname}.png")
+
 #Just_Simple_make_it_simple
